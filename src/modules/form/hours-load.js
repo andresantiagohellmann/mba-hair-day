baa402818c750656419ba9bd41ec418a8f1c60ad
Trydo Studio <trydostudio@gmail.com>
2024-10-08 16:02:44 -0300

    blocking scheduled times

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -4,20 +4,27 @@ import { hoursClick } from "./hours-click";
 
 const hours = document.getElementById("hours");
 
-export function hoursLoad({ date }) {
+export function hoursLoad({ date, dailySchedules }) {
   // Limpa a lista de horários.
   hours.innerHTML = "";
+
+  // Obtém a lista de horários ocupados.
+  const unavailableHours = dailySchedules.map((schedule) =>
+    dayjs(schedule.when).format("HH:mm")
+  );
 
   // Recupera somente a hora.
   const opening = openingHours.map((hour) => {
     const [scheduleHour] = hour.split(":");
 
     // Adiciona a hora da data e verifica se está no passado.
-    const isHourPast = dayjs(date).add(scheduleHour, "hour").isAfter(dayjs());
+    const isHourPast = dayjs(date).add(scheduleHour, "hour").isBefore(dayjs());
+
+    const available = !unavailableHours.includes(hour) && !isHourPast;
 
     return {
       hour,
-      available: isHourPast,
+      available,
     };
   });
 
---
+++ src/services/schedule-fetch-by-day.js
@@ -0,0 +1,19 @@
+import dayjs from "dayjs";
+import { apiConfig } from "./api-config";
+
+export async function scheduleFetchByDay({ date }) {
+  try {
+    const response = await fetch(`${apiConfig.baseURL}/schedules`);
+    const data = await response.json();
+
+    // Filtra agendamentos pelo dia selecionado.
+    const dailySchedules = data.filter((schedule) => {
+      return dayjs(date).isSame(schedule.when, "day");
+    });
+
+    return dailySchedules;
+  } catch (error) {
+    console.log(error);
+    alert("Não foi possível buscar os agendamentos do dia selecionado.");
+  }
+}
